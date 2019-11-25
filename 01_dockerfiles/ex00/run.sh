b3b77835b8beeacdc919785326322b5df47b97c9
till de roquefeuil <tillderoquefeuil@gmail.com>
2019-11-25 15:14:54 +0100

    run.sh ex01

---
+++ 01_dockerfiles/ex00/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+GRN="\x1B[0;32m"
+YEL="\x1B[1;33m"
+RST="\x1B[0m"
+
+echo $YEL"Docker-1 | 01_dockerfiles | ex00"$RST
+
+echo $YEL"\n\nBUILD ex00"$RST
+echo "      docker build -t ex00 .\n"
+
+echo $YEL"Build image (y) or skip (enter)?"$RST
+read RESPONSE
+if [ "$RESPONSE" = "y" ]
+then
+    docker build -t ex00 .
+    echo $GRN"\nImage successfully built"$RST
+fi
+RESPONSE="n"
+
+
+echo $YEL"\n\nRUN ex00"$RST
+echo "      docker run -it --name ex00 --rm ex00\n"
+
+echo $YEL"Run image (y) or skip (enter)?"$RST
+read RESPONSE
+if [ "$RESPONSE" = "y" ]
+then
+    docker run -it --name ex00 --rm ex00
+fi
+
+
