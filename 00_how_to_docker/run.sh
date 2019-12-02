52712295d9043f63f598010c8ab9753293de6048
till de roquefeuil <tillderoquefeuil@gmail.com>
2019-12-02 11:57:27 +0100

    how to docker OK

--- 00_how_to_docker/run.sh
+++ 00_how_to_docker/run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 YEL="\x1B[1;33m"
+RED="\x1B[1;31m"
 RST="\x1B[0m"
 
 echo $YEL"Command :"$RST
 cat $1
 echo $YEL"\n\nOutput :"$RST
 ./$1
+
+if [ $1 = '03' ]; then
+    echo $RED"\nWatch out!\nThis command can't be run like this!\nYou have to run 'eval \$(docker-machine env Char)' in your terminal\nTo see if it works, run 'docker-machine ls'"$RST
+fi
